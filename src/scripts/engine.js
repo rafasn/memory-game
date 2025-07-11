c631312e6f67aacb2f0eb6a0ecb1344d21b4a3a3
rafasn <neiva.rafs@gmail.com>
2025-07-10 22:35:39 -0300

    atualizando

---
+++ src/scripts/engine.js
@@ -0,0 +1,23 @@
+const emojis = [
+    "🍉",
+    "🍉",
+    "🍊",
+    "🍊",
+    "🍒",
+    "🍒",
+    "🍓",
+    "🍓",
+    "🍍",
+    "🍍",
+    "🥝",
+    "🥝"
+];
+let openCard = [];
+let shuffleEmojis= emojis.sort(()=>(Math.random()>0.5 ? 2:-1));
+
+for(let i=0; i < emojis.length;i++){
+    let box = document.createElement("div");
+    box.className="item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
